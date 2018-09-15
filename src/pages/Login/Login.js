a675e048f7cf423491c12ad5da8274717f53c792
Minh Quang <minhquangnguyen1996@gmail.com>
2018-09-16 02:26:12 +0700

    commit code Login/Login.js

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,11 +1,46 @@
 import React, { Component } from 'react';
+import { Button, Form, Divider } from 'semantic-ui-react'
+import styled from 'styled-components';
+import { Link } from '@reach/router';
 
+const Container = styled.div`
+    color: #000;
+    margin: 0 auto;
+    background: white;
+    padding: 15px;
+    font-weight: bold;
+    text-align: center;
+    vertical-align: middle;
+    margin-top: 20px;
+`
+
+const FormLogin = styled.div`
+    font-size: 24px;
+`
+
+const SignUp = styled.div`
+    font-size: 15px;
+    margin-top:20px;
+`
 class Login extends Component {
     render() {
         return (
-            <div>
-                
-            </div>
+            <Container>
+                <FormLogin>
+                    <p>Login</p>
+                    <Form>
+                        <Form.Input placeholder='Username' required />
+                        <Form.Input type="password" placeholder='Password' required />
+                        <Button primary fluid>Submit</Button>
+                    </Form>
+                </FormLogin>
+                <Divider />
+                <SignUp>
+                    <p>Don't have a account?
+                        <Link to="/register">Sign Up</Link>
+                    </p>
+                </SignUp>
+            </Container>
         );
     }
 }
